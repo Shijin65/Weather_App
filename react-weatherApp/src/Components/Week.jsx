eec35fe73fc831b06c189e48d24e6304625677ea
shijin p <shijincht65@gmail.com>
2023-10-25 22:09:50 +0530

    card responcive

--- react-weatherApp/src/Components/Week.jsx
+++ react-weatherApp/src/Components/Week.jsx
@@ -3,25 +3,26 @@ const card=[1,1,1,1,1,1,1]
 function Week() {
   return (
     
-        <div className="px-10 w-full ">
+        <div className="md:px-10 w-full ">
               <div className="text-lg sm:text-2xl  border-b-4  border-white border-opacity-50 w-full">
                 <h1>This Week</h1>
               </div>
-              <div className="grid grid-flow-col grid-cols-2  gap-5 flex-wrap  bg-black overflow-x-scroll ">
+              <div className="grid grid-flow-col md:grid-rows-1 grid-rows-2 items-center gap-5 flex-wrap  md:overflow-x-scroll lg:overflow-x-hidden ">
                 {/* card */}
                 {card.map((item) => (
-                  <div className=" card flex-col pt-3 md:w-28 sm:w-auto items-center bg-white border-white border-2 rounded-md bg-opacity-20 mt-3 text-center overflow-hidden">
+                  <span>
+                  <div className=" card flex-col pt-3 md:w-28  sm:w-auto items-center bg-white border-white border-2 rounded-md bg-opacity-50 text-slate-600 mt-3 text-center overflow-hidden">
                     <h1>Monday</h1>
                     <div className="flex justify-center">
                       <img
                         src="images/clouds.png"
                         alt=""
                         srcset=""
-                        className="h-20"
+                        className="md:h-20"
                       />
                     </div>
                     <h4>00/00°C</h4>
-                  </div>
+                  </div></span>
                 ))}
               </div>
             </div>
